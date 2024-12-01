396bf44e3770a91888cd36c7940319e0f5dfa6f9
dduongdev <nguyendongduong4551190010@gmail.com>
2024-12-01 19:59:55 +0700

    update units

--- UseCases/IBookManager.cs
+++ UseCases/IBookManager.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,10 @@ namespace UseCases
 {
     public interface IBookManager
     {
+        Task<IEnumerable<Book>> GetAllAsync();
+        Task<Book?> GetByIdAsync(int id);
+        Task AddAsync(Book book);
+        Task UpdateAsync(Book book);
+        Task SuspendAsync(int id);
     }
 }
--- UseCases/ICartManager.cs
+++ UseCases/ICartManager.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,11 @@ namespace UseCases
 {
     public interface ICartManager
     {
+        Task<IEnumerable<Cart>> GetAllAsync();
+        Task<Cart?> GetByIdAsync(int id);
+        Task<Cart?> GetByUserIdAsync(int userId);
+        Task AddAsync(Cart cart);
+        Task UpdateAsync(Cart cart);
+        Task SuspendAsync(int id);
     }
 }
